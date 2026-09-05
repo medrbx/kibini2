de95fd250a49972fe5bb0e903c1b0c91ef459f0d
François Pichenot <fpichenot@ville-roubaix.fr>
2026-09-05 12:56:54 +0200

    MaJ

--- kibini/webapp/static/javascripts/scripts.js
+++ kibini/webapp/static/javascripts/scripts.js
@@ -7,3 +7,19 @@ $('[data-toggle=collapse]').click(function(){
   	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
 });
 });
+
+/* Ajuste la hauteur d'un iframe de tableau de bord sur celle de son contenu réel
+   (même origine : /static/data/*.html, servi par cette même appli Flask).
+   En cas d'échec (contenu pas encore chargé, structure inattendue...), la
+   hauteur définie dans webapp/dashboards.py reste affichée telle quelle. */
+function autoResizeIframe(iframe) {
+    try {
+        var doc = iframe.contentWindow.document;
+        var height = Math.max(doc.documentElement.scrollHeight, doc.body.scrollHeight);
+        if (height) {
+            iframe.style.height = height + "px";
+        }
+    } catch (e) {
+        // contenu cross-origin ou non accessible : on garde la hauteur par défaut
+    }
+}
