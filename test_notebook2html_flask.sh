0615e5f6517eefa6a5be6d34f0ec8c350561f756
François Pichenot <fpichenot@ville-roubaix.fr>
2026-09-09 10:30:49 +0200

    MaJ

--- test_notebook2html_flask.sh
+++ test_notebook2html_flask.sh
@@ -2,8 +2,10 @@
 
 
 date=$(date '+%Y-%m-%d')
-kibini2='/home/kibini/kibini2'
-kibini='/home/kibini/kibini_prod'
+kibini2="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+data_dir="$kibini2/kibini/webapp/static/data"
+mkdir -p "$data_dir"
+cd "$kibini2"
 
 # Execution et publication d'un notebook en particulier
 for filename in notebook_dimanche26
@@ -13,6 +15,6 @@ do
     newfilename="$filename"_"$date"
     jupyter nbconvert --to notebook --execute kibini/$filename.ipynb --output $newfilename
     jupyter nbconvert --to html --no-input kibini/$newfilename.ipynb
-    cp kibini/$newfilename.html /home/kibini/kibini_prod/public/data/$filename.html
+    cp kibini/$newfilename.html $data_dir/$filename.html
     rm kibini/$newfilename*
 done
